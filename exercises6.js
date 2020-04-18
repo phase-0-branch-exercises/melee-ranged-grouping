6e56444388f6a4abb9d587ec8a312acb83dd486d
widyo05 <widyo05>
2020-04-18 16:52:47 +0700

    exercises 6

---
+++ exercises6.js
@@ -0,0 +1,32 @@
+function meleeRangedGrouping(str) {
+    var str = str.split(',')
+    var tmp = []
+
+    for (var i = 0; i < str.length; i++) {
+        tampung = str[i].split('-')
+        tmp.push(tampung)
+    }
+    // if (tmp.length == 1) {
+    //     return []
+    // }
+    // var rangedArr = []
+    // var meleeArr = []
+    // var hasil = []
+    // for (var i = 0; i < tmp.length; i++) {
+    //     if (tmp[i][1] === 'Ranged') {
+    //         rangedArr.push(tmp[i][0])
+    //     } else if (tmp[i][1] === 'Melee') {
+    //         meleeArr.push(tmp[i][0])
+    //     }
+    // }
+    // hasil.push(rangedArr, meleeArr)
+    return tmp
+}
+
+console.log(meleeRangedGrouping('Razor-Ranged,Invoker-Ranged,Meepo-Melee,Axe-Melee,Sniper-Ranged'));
+// [ ['Razor', 'Invoker', 'Sniper'], ['Meepo', 'Axe'] ]
+
+console.log(meleeRangedGrouping('Drow Ranger-Ranged,Chen-Ranged,Dazzle-Ranged,Io-Ranged'));
+// [ ['Drow Ranger', 'Chen', 'Dazzle', 'Io'], [] ]
+
+console.log(meleeRangedGrouping('')); // []
