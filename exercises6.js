b93a983fd8093da4861ae46e76c0055dbc953940
widyo05 <widyo05>
2020-04-18 21:30:36 +0700

    perubahan exercises 6

--- exercises6.js
+++ exercises6.js
@@ -6,21 +6,21 @@ function meleeRangedGrouping(str) {
         tampung = str[i].split('-')
         tmp.push(tampung)
     }
-    // if (tmp.length == 1) {
-    //     return []
-    // }
-    // var rangedArr = []
-    // var meleeArr = []
-    // var hasil = []
-    // for (var i = 0; i < tmp.length; i++) {
-    //     if (tmp[i][1] === 'Ranged') {
-    //         rangedArr.push(tmp[i][0])
-    //     } else if (tmp[i][1] === 'Melee') {
-    //         meleeArr.push(tmp[i][0])
-    //     }
-    // }
-    // hasil.push(rangedArr, meleeArr)
-    return tmp
+    if (tmp.length == 1) {
+         return []
+     }
+     var rangedArr = []
+     var meleeArr = []
+     var hasil = []
+     for (var i = 0; i < tmp.length; i++) {
+        if (tmp[i][1] === 'Ranged') {
+             rangedArr.push(tmp[i][0])
+         } else if (tmp[i][1] === 'Melee') {
+             meleeArr.push(tmp[i][0])
+         }
+     }
+     hasil.push(rangedArr, meleeArr)
+    return hasil
 }
 
 console.log(meleeRangedGrouping('Razor-Ranged,Invoker-Ranged,Meepo-Melee,Axe-Melee,Sniper-Ranged'));
